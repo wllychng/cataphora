f15aac51df2e24fd1b292486a1d5c972daebb3b0
Willy Cheung <cheung.179@osu.edu>
2022-08-29 15:05:06 -0400

    fixed bug with stimuli cleaning

--- code/stimuli_utils.py
+++ code/stimuli_utils.py
@@ -6,9 +6,11 @@ import pandas as pd
 def clean_stimuli(df):
     # delete rows where stimuli is NaN
     assert "example" in df.columns
-    df.dropna(subset = "example", inplace = True)
+    df.dropna(subset = ["example"], inplace = True)
     return df
 
 if __name__ == "__main__":
     # test clean_stimuli function
-    print("TODO")
+    test_df = pd.DataFrame({"col1": [1, 2, 3], "example": ["A", None, "C"]})
+    test_df = clean_stimuli(test_df)
+    print(test_df)
